d54cde084cf139fa8d8dba2f5d8d3b6c1f2b5dce
alceronDelgado <luis_aceron@soy.sena.edu.co>
2024-07-23 19:38:17 -0500

    arreglado indentación

--- main.py
+++ main.py
@@ -36,95 +36,93 @@ color_texto = (255,255,255) #Color del texto en BGR
 grosor = 2 #grosor de la letra o en inglés conocido como = thickness, se mide en pixeles 2 = 2px
 
 while True:
-        try:
-            # Lectura de la cámara
-            ret, frame = video.read()
-            if ret == False:
-                break
-            else:
-                #Extraemos con librería numpy la altura, ancho
-                alto, ancho, c = frame.shape
-                
-                #Sacar el tamaño del eje x
-                x1 = int(ancho / 3)
-                x2 = int(x1 * 2)
-                
-                #tamaño eje y
-                y1 = int(alto / 4)
-                y2 = int(y1 * 2)
-
-                #Ubicamos el rectánculo en la zona específica 
-                
-                #Agregamos texto TODO: Esto para después de que haya leído el objeto
-                
-                #Pasamos Imágen a escala de grises
-                frame_gray = cv.cvtColor(frame,cv.COLOR_BGR2GRAY)
-                
-                #Suavisamos el filtro ya que los pixeles cambian muy rápido
-                frame_gray = cv.GaussianBlur(frame_gray,(1,1),0,0)
-                
-                _, frame_gray = cv.threshold(frame_gray,155,255,cv.THRESH_BINARY)
-                #_, frame_gray = cv.threshold(frame_gray,200,255,cv.THRESH_BINARY_INV)
-                
-                #buscamos los contornos con los parametros = De la función findcontours = devuelve 2 valores, valoresHerados y el contorno
-                """"
-                img = la imagen o video que queremos que encuentre los contornos
-                mode = es el modo de recuperación del contorno = usaremos para este ejemplo el de recuperar todos los contornos = cv.RETR_TREE, nos muestra los contornos internos y externos
-                method = método de aproximación de contorno, hay 2 formas = CHAIN_APPROX_SIMPLE y CHAIN_APPROX_NONE = usaremos el none que encuentra todos los puntos del contorno, CHAIN APROX_SIMPLE = Encuentra solo los elementos de las puntas
-                """
-                ##Nos devuelve 2 cosas, el contorno y la jerarquía de los contornos
-                contorno, _ = cv.findContours(frame_gray, cv.RETR_TREE,cv.CHAIN_APPROX_SIMPLE)
-                
-                
-                #Dibujamos un triángulo en el centro
-                #frame_2 = cv.rectangle(frame,(x1,y1),(x2,y2),(0,255,0),3)
-                #recorte = frame_2[y1:y2, x1:x2]
-                for c in contorno:
-                    #c es el numpy array de los puntos de todo el contorno
-                    area = cv.contourArea(c)
-                    (x,y,w,h) = cv.boundingRect(c)
-                    #En este proceso tratamos de encontrar esclusivamente las figuras rectangulares y que no tengan curvas, el argumento true determina si la forma es un contorno cerrado y no una curva
-                    cuadrado = 0.09 * cv.arcLength(c, True)
-                    aproximado = cv.approxPolyDP(c, cuadrado, True)
-                    
-                    # Len aproximado == 4 significa si el area contiene 4 vertices
-                    if len(aproximado)== 4 and area > 8000:
-                        #print("area=",area)
-                        #frame = cv.rectangle(frame,(x,y),(x + w , y + h),(0,255,0),1)
-                        #frame = cv.drawContours(frame,[c],0,(0,255,0),3,cv.LINE_AA)
-                        
-                        #Usarmos el aspect radio para determinar el contorno y saber que se trate de un rectángulo
-                        aspect_radio = float(300.6)/151.2
-                        #print("aspect radio=",aspect_radio)
-                        if aspect_radio > 1.5 and aspect_radio < 2.5: 
-                            frame = cv.drawContours(frame,[c],0,(0,255,0),3,cv.LINE_AA)
-                            # guardamos el area de la placa, para eso vamos a usar los contornos encontrados con boundingRect
-                            placa_figura = frame_gray[y:y + h, x:x + w]
-                            #Reconocer caracteres - usaremos pyteseract
-                            """
-                            Parámetros
-                            placa figura = corresponde a lo que queremos leer
-                            config = modo de segmentación de página, usaremos la forma --psm 11, podemos probar con otras pero el resultado de conocimiento de caracteres puede cambiar
-                            """
-                            texto = pytesseract.image_to_string(placa_figura,config='--psm 1')
-                            if len(texto) == 7:
-                                print("texto placa ",texto)
-                            
-                cv.imshow('imagen',frame)
-                cv.imshow('placa',placa_figura)
-                if cv.waitKey(1) & 0xFF == ord('c'):
-                    print("Sales del programa")
-                    #Guarda una captura de lo último que ve antes de salir del programa
-                    ruta_imagen = f"img/imagen{i+1}.jpg"
-                    imagen = cv.imwrite(ruta_imagen,frame)
-                    i = i + 1
-                    break
-        except KeyboardInterrupt:
-            print("sales del programa por fuerza mayor, presionaste CTRL + C")
-            ruta_imagen = f"img/imagen_{i+1}.jpg"
-            imagen = cv.imwrite(ruta_imagen,frame)
-            i = i + 1
+    try:
+        # Lectura de la cámara
+        ret, frame = video.read()
+        if ret == False:
             break
+        else:
+            #Extraemos con librería numpy la altura, ancho
+            alto, ancho, c = frame.shape
+                
+            #Sacar el tamaño del eje x
+            x1 = int(ancho / 3)
+            x2 = int(x1 * 2)
+                
+            #tamaño eje y
+            y1 = int(alto / 4)
+            y2 = int(y1 * 2)
+
+            #Ubicamos el rectánculo en la zona específica 
+                
+            #Agregamos texto TODO: Esto para después de que haya leído el objeto
+                
+            #Pasamos Imágen a escala de grises
+            frame_gray = cv.cvtColor(frame,cv.COLOR_BGR2GRAY)
+                
+            #Suavisamos el filtro ya que los pixeles cambian muy rápido
+            frame_gray = cv.GaussianBlur(frame_gray,(1,1),0,0)
+                
+            _, frame_gray = cv.threshold(frame_gray,155,255,cv.THRESH_BINARY)
+            #_, frame_gray = cv.threshold(frame_gray,200,255,cv.THRESH_BINARY_INV)
+                
+            #buscamos los contornos con los parametros = De la función findcontours = devuelve 2 valores, valoresHerados y el contorno
+            """"
+            img = la imagen o video que queremos que encuentre los contornos
+            mode = es el modo de recuperación del contorno = usaremos para este ejemplo el de recuperar todos los contornos = cvRETR_TREE, nos muestra los contornos internos y externos
+            method = método de aproximación de contorno, hay 2 formas = CHAIN_APPROX_SIMPLE y CHAIN_APPROX_NONE = usaremos el none queencuentra todos los puntos del contorno, CHAIN APROX_SIMPLE = Encuentra solo los elementos de las puntas
+            """
+            ##Nos devuelve 2 cosas, el contorno y la jerarquía de los contornos
+            contorno, _ = cv.findContours(frame_gray, cv.RETR_TREE,cv.CHAIN_APPROX_SIMPLE)
+                
+                
+            #Dibujamos un triángulo en el centro
+            #frame_2 = cv.rectangle(frame,(x1,y1),(x2,y2),(0,255,0),3)
+            #recorte = frame_2[y1:y2, x1:x2]
+            for c in contorno:
+                #c es el numpy array de los puntos de todo el contorno
+                area = cv.contourArea(c)
+                (x,y,w,h) = cv.boundingRect(c)
+                #En este proceso tratamos de encontrar esclusivamente las figuras rectangulares y que no tengan curvas, el argumento truedetermina si la forma es un contorno cerrado y no una curva
+                cuadrado = 0.09 * cv.arcLength(c, True)
+                aproximado = cv.approxPolyDP(c, cuadrado, True)
+                # Len aproximado == 4 significa si el area contiene 4 vertices
+                if len(aproximado)== 4 and area > 8000:
+                    #print("area=",area)
+                    #frame = cv.rectangle(frame,(x,y),(x + w , y + h),(0,255,0),1)
+                    #frame = cv.drawContours(frame,[c],0,(0,255,0),3,cv.LINE_AA)
+                    #Usarmos el aspect radio para determinar el contorno y saber que se trate de un rectángulo
+                    aspect_radio = float(300.6)/151.2
+                    #print("aspect radio=",aspect_radio)
+                    if aspect_radio > 1.5 and aspect_radio < 2.5: 
+                        frame = cv.drawContours(frame,[c],0,(0,255,0),3,cv.LINE_AA)
+                        # guardamos el area de la placa, para eso vamos a usar los contornos encontrados con boundingRect
+                        placa_figura = frame_gray[y:y + h, x:x + w]
+                        #Reconocer caracteres - usaremos pyteseract
+                        """
+                        Parámetros
+                        placa figura = corresponde a lo que queremos leer
+                        config = modo de segmentación de página, usaremos la forma --psm 11, podemos probar con otras pero el resultadode conocimiento de caracteres puede cambiar
+                        """
+                        texto = pytesseract.image_to_string(placa_figura,config='--psm 1')
+                        if len(texto) == 7:
+                            print("texto placa ",texto)
+                            
+            cv.imshow('imagen',frame)
+            cv.imshow('placa',placa_figura)
+            if cv.waitKey(1) & 0xFF == ord('c'):
+                print("Sales del programa")
+                #Guarda una captura de lo último que ve antes de salir del programa
+                ruta_imagen = f"img/imagen{i+1}.jpg"
+                imagen = cv.imwrite(ruta_imagen,frame)
+                i = i + 1
+                break
+    except KeyboardInterrupt:
+        print("sales del programa por fuerza mayor, presionaste CTRL + C")
+        ruta_imagen = f"img/imagen_{i+1}.jpg"
+        imagen = cv.imwrite(ruta_imagen,frame)
+        i = i + 1
+        break
 
 cv.waitKey(1)
 video.release() #Cerramos video
